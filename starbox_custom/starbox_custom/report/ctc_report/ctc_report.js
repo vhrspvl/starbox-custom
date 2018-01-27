3139f328b8d13394539c75eff66122711ba13997
Abdulla P I <hereabdulla@gmail.com>
2018-01-27 10:53:12 +0530

    updated ctc report

--- starbox_custom/starbox_custom/report/ctc_report/ctc_report.js
+++ starbox_custom/starbox_custom/report/ctc_report/ctc_report.js
@@ -5,24 +5,18 @@
 frappe.query_reports["CTC Report"] = {
 	"filters": [
 		{
-			"fieldname": "date",
-			"label": __("Date"),
+			"fieldname": "from_date",
+			"label": __("From Date"),
 			"fieldtype": "Date",
-			"default": frappe.datetime.get_today(),
+			"default": [frappe.datetime.add_months(frappe.datetime.get_today())],
 			"reqd": 1
 		},
 		{
-			"fieldname": "employee",
-			"label": __("Employee"),
-			"fieldtype": "Link",
-			"options": "Employee"
-		},
-		{
-			"fieldname": "company",
-			"label": __("Company"),
-			"fieldtype": "Link",
-			"options": "Company",
-			"default": frappe.defaults.get_user_default("Company")
+			"fieldname": "to_date",
+			"label": __("To Date"),
+			"fieldtype": "Date",
+			"default": [frappe.datetime.add_months(frappe.datetime.get_today())],
+			"reqd": 1
 		}
 	]
 }
--- starbox_custom/starbox_custom/report/ctc_report/ctc_report.py
+++ starbox_custom/starbox_custom/report/ctc_report/ctc_report.py
@@ -3,7 +3,7 @@
 
 from __future__ import unicode_literals
 import frappe
-from frappe.utils import cstr, cint, getdate, today,nowdate
+from frappe.utils import cstr, cint, getdate, today, nowdate
 from frappe import msgprint, _
 
 
@@ -16,20 +16,28 @@ def execute(filters=None):
 
     data = []
     row = []
-    for att in frappe.get_list('Attendance', fields=['name', 'employee'], filters={'attendance_date': date, 'docstatus': 1, 'status': 'Present'}):
-        emp = frappe.db.get_value("Employee", {'employee': att.employee}, [
-                                  'name', 'employee_name', 'designation', 'department'], as_dict=True)
-        row = [att.name, emp.employee_name, emp.designation, emp.department]
-        ss_base = frappe.db.get_value("Salary Structure Employee", {'employee': emp.name}, [
+    active_employees = get_active_employees()
+    for emp in active_employees:
+        row = [emp.name, emp.employee_name, emp.designation, emp.department]
+
+        base = frappe.db.get_value("Salary Structure Employee", {'employee': emp.name}, [
             'base'], as_dict=True)
+        if base:
+            row += [base.base]
+        else:
+             row += [""]
 
-	if ss_base:
-		if ss_base.base:
-			row += [ss_base.base]
-		else:
-			row += [""]
+        present_days = 0
+        emp_present_days = get_employee_attendance(emp.name, filters)
 
-    data.append(row)
+        for present in emp_present_days:
+            present_days = present.count
+        if emp_present_days:
+            row += [present_days]
+        else:
+            row += ["0"]
+
+        data.append(row)
 
     return columns, data
 
@@ -40,33 +48,25 @@ def get_columns(attendance):
         _("Employee Name") + "::150",
         _("Designation") + "::180",
         _("Department") + "::180",
-        _("Gross") + ":Currency:180"
+        _("Gross") + ":Currency:180",
+        _("PD") + "::50"
     ]
     return columns
 
 
-def get_attendance(filters):
-    filters.update({"from_date": filters.get("date_range")[
-                   0], "to_date": filters.get("date_range")[1]})
+def get_active_employees():
+    active_employees = frappe.db.sql(
+        """select * from `tabEmployee` where status = "Active" order by name""", as_dict=1)
+    return active_employees
 
-    conditions, filters = get_conditions(filters)
-    # attendance = frappe.db.sql(
-    # 	"""select
-    # """)
-    # attendance = frappe.db.sql(
-    # 	"""select emp.name,emp.designation,emp.department,sse.base
-    # 	from `tabEmployee` emp,`tabSalary Structure Employee` sse
-    # 	where emp.status='Active' and sse.employee =
-    # 	order by employee""" % date as_dict=1)
+
+def get_employee_attendance(employee, filters):
+    employee_attendance = frappe.db.sql("""select count(*) as count from `tabAttendance` where \
+        docstatus = 1 and status = 'Present' and employee= %s and attendance_date between %s and %s""", (employee, filters.get("from_date"), filters.get("to_date")), as_dict=1)
+    return employee_attendance
 
 
 def get_conditions(filters):
     conditions = ""
     if filters.get("date_range"):
-        conditions += " and attendance_date between %(from_date)s and %(to_date)s"
-    if filters.get("company"):
-        conditions += " and company = %(company)s"
-    if filters.get("employee"):
-        conditions += " and employee = %(employee)s"
-
-    return conditions, filters
+        conditions += " "
