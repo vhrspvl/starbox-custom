6bcddeeb6e6c3b2fe88335bb66cf6e7397ef3170
Abdulla P I <hereabdulla@gmail.com>
2018-01-29 19:13:30 +0530

    updated ctc report

--- starbox_custom/starbox_custom/report/ctc_report/ctc_report.py
+++ starbox_custom/starbox_custom/report/ctc_report/ctc_report.py
@@ -2,9 +2,11 @@
 # For license information, please see license.txt
 
 from __future__ import unicode_literals
+
 import frappe
-from frappe.utils import cstr, cint, flt, getdate, today, nowdate, rounded, date_diff, money_in_words
-from frappe import msgprint, _
+from frappe import _, msgprint
+from frappe.utils import (cint, cstr, date_diff, flt, getdate, money_in_words,
+                          nowdate, rounded, today)
 
 
 def execute(filters=None):
@@ -17,7 +19,7 @@ def execute(filters=None):
     row = []
     active_employees = get_active_employees()
     for emp in active_employees:
-        row = [emp.name, emp.employee_name, emp.designation, emp.department]
+        row = [emp.name, emp.employee_name, emp.designation, emp.department,emp.employment_type]
 
         emp_present_days = get_employee_attendance(emp.name, filters)
 
@@ -34,66 +36,107 @@ def execute(filters=None):
             gross = sse.base    
             if gross:
                 row += [gross]
-                if present_days > 0:
-                    
-                    act_basic = (flt(gross) * .35)
-                    daily_basic = (act_basic / flt('26'))
-                    act_hra = (flt(gross) * .35)
-                    daily_hra = (act_hra / flt('26'))
-                    act_da = (flt(gross) * .25)
-                    daily_da = (act_da / flt('26'))
-                    act_ca = flt("1600")
-                    daily_ca = flt(act_ca) / flt('26')
-                    act_ma = flt("1250")
-                    daily_ma = flt(act_ma) / flt('26')
-                    act_wa = flt("150")
-                    daily_ma = flt(act_wa) / flt('26')
-                    act_oa = flt(gross)-(act_basic + act_ca + act_ma)
+
+                act_basic = (flt(gross) * .35)
+                daily_basic = (act_basic / flt('26'))
+                act_hra = (flt(gross) * .35)
+                daily_hra = (act_hra / flt('26'))
+                act_da = (flt(gross) * .25)
+                daily_da = (act_da / flt('26'))
+                act_wa = flt("150")
+                daily_wa = (act_wa / flt('26'))
+                act_ca = flt("1600")
+                daily_ca = flt(act_ca) / flt('26')
+                act_ma = flt("1250")
+                daily_ma = flt(act_ma) / flt('26')
+                act_wa = flt("150")
+                daily_ma = flt(act_wa) / flt('26')
+                if emp.employment_type == 'Staff':
+                    act_oa = flt(gross) - (act_basic + act_hra + act_ca + act_ma)
                     daily_oa = flt(act_oa) / flt('26')
+                if emp.employment_type == 'Operator':
+                    act_oa = flt(gross) - (act_basic + act_hra +
+                                           act_wa + act_da)
+                    daily_oa = flt(act_oa) / flt('26')
+
+                total_actuals = 0
+                if act_basic:
+                    row += [act_basic]
+                    total_actuals += act_basic 
+                if act_hra:
+                    row += [act_hra]
+                    total_actuals += act_hra
+                if emp.employment_type == 'Operator':
+                    if act_da:
+                        row += [act_da]
+                        total_actuals += act_da
+                    else:
+                        row += [""]
+                    if act_wa:
+                        row += [act_wa]
+                        total_actuals += act_wa
+                    else:
+                        row += [""]
+                else:
+                    row += ["",""]
+                if emp.employment_type == 'Staff':
+                    if act_ca:
+                            row += [act_ca]
+                            total_actuals += act_ca
+                    if act_ma:
+                            row += [act_ma]
+                            total_actuals += act_ma
+                else:
+                    row += ["", ""]
+                if act_oa:
+                    row += [act_oa]
+                    total_actuals += act_oa
+
+                if total_actuals:
+                    row += [total_actuals] 
+                else:
+                    row += [""]    
+
+                if present_days > 0:
                     earned_basic = flt(daily_basic) * flt(present_days)
                     earned_hra = flt(daily_hra) * flt(present_days)
                     earned_da = flt(daily_da) * flt(present_days)
+                    earned_wa = flt(daily_wa) * flt(present_days)
                     earned_ca = flt(daily_ca) * flt(present_days)
                     earned_ma = flt(daily_ma) * flt(present_days)
                     earned_oa = flt(daily_oa) * flt(present_days)
-                    da = 0
-                    total_earnings = flt(earned_basic + earned_ca + earned_da + earned_hra + earned_ma + earned_oa)
-                    if emp.employment_type == 'Operator' or emp.employment_type == 'Foreman':
-                        pf = (flt(daily_basic + daily_da)*0.12)*flt(present_days)
-                        da = 1
-                    else:
-                        pf = (flt(daily_basic) * 0.12) * flt(present_days)
-                    if act_basic:
-                        row += [act_basic]
-                    if act_hra:
-                        row += [act_hra]
-                    if da:
-                        if act_da:
-                            row += [act_da]
-                        else:
-                            row += [""]    
-                    if act_ca:
-                            row += [act_ca]
-                    if act_ma:
-                            row += [act_ma]
-                    if act_oa:
-                        row += [act_oa]
+                    total_earnings = 0
                     if earned_basic:
                         row += [earned_basic]
+                        total_earnings += earned_basic
                     if earned_hra:
                         row += [earned_hra]
-                    if earned_da:
-                        row += [earned_da]
-                    if earned_ca:
-                        row += [earned_ca]
-                    if earned_ma:
-                        row += [earned_ma]
+                        total_earnings += earned_hra
+                    if emp.employment_type == 'Operator':
+                        if earned_da:
+                            row += [earned_da]
+                            total_earnings += earned_da
+                        if earned_wa:
+                            row += [earned_wa]
+                            total_earnings += earned_wa
+                    else:
+                        row += ["",""] 
+                    if emp.employment_type == 'Staff':
+                        if earned_ca:
+                            row += [earned_ca]
+                            total_earnings += earned_ca
+                        if earned_ma:
+                            row += [earned_ma]
+                            total_earnings += earned_ma
+                    else:
+                        row += ["", ""]
                     if earned_oa:
                         row += [earned_oa]
+                        total_earnings += earned_oa
                     if total_earnings:
                         row += [total_earnings]
-            else:
-                row += [""]
+                else:
+                    row += [""]
 
         
         
@@ -109,17 +152,21 @@ def get_columns(attendance):
         _("Employee Name") + "::150",
         _("Designation") + "::180",
         _("Department") + "::180",
+        _("Employment Type") + "::180",
         _("PD") + "::50",
         _("Gross") + ":Currency:100",
         _("Actual Basic") + ":Currency:100",
         _("Actual HRA") + ":Currency:100",
         _("Actual DA") + ":Currency:100",
+        _("Actual WA") + ":Currency:100",
         _("Actual CA") + ":Currency:100",
         _("Actual MA") + ":Currency:100",
         _("Actual OA") + ":Currency:100",
+        _("Actual Totals") + ":Currency:100",
         _("Earned Basic") + ":Currency:100",
         _("Earned HRA") + ":Currency:100",
         _("Earned DA") + ":Currency:100",
+        _("Earned WA") + ":Currency:100",
         _("Earned CA") + ":Currency:100",
         _("Earned MA") + ":Currency:100",
         _("Earned OA") + ":Currency:100",
