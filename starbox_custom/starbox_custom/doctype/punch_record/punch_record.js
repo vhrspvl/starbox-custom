f79ca4d69211329b8aec6ff0042c8c59f52cb3c8
hereabdulla <hereabdulla@gmail.com>
2018-03-26 15:08:31 +0530

    added doctype to get punch record

---
+++ starbox_custom/starbox_custom/doctype/punch_record/punch_record.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2018, Starboxes India and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on('Punch Record', {
+	refresh: function(frm) {
+
+	}
+});
