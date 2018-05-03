ece5659fd88026db4273e49048b21e7e73c3a106
hereabdulla <hereabdulla@gmail.com>
2018-05-03 10:47:47 +0530

    updated custom for    delete lop afterrrr present

--- starbox_custom/custom.py
+++ starbox_custom/custom.py
@@ -214,29 +214,25 @@ def emp_absent_today():
 @frappe.whitelist()
 def removelop():
     # day = add_days(today(), -1)
-    # days = ["2018-04-01", "2018-04-02", "2018-04-03", "2018-04-04", "2018-04-05", "2018-04-06", "2018-04-07", "2018-04-08", "2018-04-09", "2018-04-10", "2018-04-11", "2018-04-12", "2018-04-13", "2018-04-14",
-    #         "2018-04-15", "2018-04-16", "2018-04-17", "2018-04-18", "2018-04-19", "2018-04-20", "2018-04-21", "2018-04-22", "2018-04-23", "2018-04-24", "2018-04-25", "2018-04-26", "2018-04-27", "2018-04-28", "2018-04-29", "2018-04-30"]
-    # day = datetime.strptime('19042018', "%d%m%Y").date()
-    # for day in days:
     employees = frappe.get_all('Employee', fields=['employment_type'], filters={
                                "status": "Active", "employment_type": ("!=", "Contract")})
-
-    for emp in employees:
-        print emp.employment_type
-        # query = """SELECT emp.name FROM `tabAttendance` att, `tabEmployee` emp
-        # WHERE att.employee = emp.name AND att.status='Present' AND att.attendance_date = '%s'""" % (day)
-        # present_emp = frappe.db.sql(query, as_dict=True)
-        # if emp in present_emp:
-        #     doc = frappe.get_doc('Employee', emp)
-        #     leave = get_leave(doc.name, day)
-        #     if leave:
-        #         for l in leave:
-        #             lop = frappe.get_doc("Leave Application", l)
-        #             if lop.leave_type == 'Leave Without Pay':
-        #                 print lop.employee_name
-        #                 print lop.start_date
-        # lop.db_set("docstatus", "Cancelled")
-        # frappe.delete_doc("Leave Application", lop.name)
+    days = ["2018-04-01", "2018-04-02", "2018-04-03", "2018-04-04", "2018-04-05", "2018-04-06", "2018-04-07", "2018-04-08", "2018-04-09", "2018-04-10", "2018-04-11", "2018-04-12", "2018-04-13", "2018-04-14",
+            "2018-04-15", "2018-04-16", "2018-04-17", "2018-04-18", "2018-04-19", "2018-04-20", "2018-04-21", "2018-04-22", "2018-04-23", "2018-04-24", "2018-04-25", "2018-04-26", "2018-04-27", "2018-04-28", "2018-04-29", "2018-04-30"]
+    # day = datetime.strptime('19042018', "%d%m%Y").date()
+    for day in days:
+        for emp in employees:
+            query = """SELECT emp.name FROM `tabAttendance` att, `tabEmployee` emp
+            WHERE att.employee = emp.name AND att.status='Present' AND att.attendance_date = '%s'""" % (day)
+            present_emp = frappe.db.sql(query, as_dict=True)
+            if emp in present_emp:
+                doc = frappe.get_doc('Employee', emp)
+                leave = get_leave(doc.name, day)
+                if leave:
+                    for l in leave:
+                        lop = frappe.get_doc("Leave Application", l)
+                        if lop.leave_type == 'Leave Without Pay':
+                            lop.db_set("docstatus", "Cancelled")
+                            frappe.delete_doc("Leave Application", lop.name)
 
 
 @frappe.whitelist()
