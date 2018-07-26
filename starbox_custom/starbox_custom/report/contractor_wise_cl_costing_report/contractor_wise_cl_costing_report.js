5aa82fc1e4663ddf9cf594b66b083f807a3046ff
vhrsramya <ramya.a@voltechgroup.com>
2018-07-26 11:37:51 +0530

    Added Contractor & Dep Wise CL Costing

---
+++ starbox_custom/starbox_custom/report/contractor_wise_cl_costing_report/contractor_wise_cl_costing_report.js
@@ -0,0 +1,30 @@
+// Copyright (c) 2016, Minda Sai Pvt LTd and contributors
+// For license information, please see license.txt
+/* eslint-disable */
+
+frappe.query_reports["Contractor wise CL Costing Report"] = {
+	"filters": [
+		{
+			"fieldname": "filtertype",
+			"label": __("Filter"),
+			"fieldtype": "Select",
+			"options": ["contractor", "department"],
+			// "default":"Contractor",
+			"reqd": 1
+        },
+		{
+            "fieldname": "from_date",
+            "label": __("From Date"),
+            "fieldtype": "Date",
+            "default": frappe.datetime.get_today(),
+            "reqd": 1
+        },
+        {
+            "fieldname": "to_date",
+            "label": __("To Date"),
+            "fieldtype": "Date",
+            "default": frappe.datetime.get_today(),
+            "reqd": 1
+        },
+    ],
+}
